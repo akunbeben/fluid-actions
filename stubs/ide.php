25f5cae4eb0510e04a8c4f8c99268a1019799873
Benny Rahmat <akunbeben@gmail.com>
2026-06-04 04:57:15 +0700

    feat: add IDE helper stubs

---
+++ stubs/ide.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Filament\Actions {
+    /**
+     * @method $this inlineConfirmation(?string $label = null, int $timeout = 3000)
+     * @method bool isInlineConfirmationEligible()
+     */
+    class Action {}
+}
+
+namespace Filament\Tables\Actions {
+    /**
+     * @method $this inlineConfirmation(?string $label = null, int $timeout = 3000)
+     * @method bool isInlineConfirmationEligible()
+     */
+    class Action {}
+}
+
+namespace Filament\Infolists\Components\Actions {
+    /**
+     * @method $this inlineConfirmation(?string $label = null, int $timeout = 3000)
+     * @method bool isInlineConfirmationEligible()
+     */
+    class Action {}
+}
