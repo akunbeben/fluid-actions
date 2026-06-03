1bc77844b4e8222edc894a73213718fb6e77c5f0
Benny Rahmat <akunbeben@gmail.com>
2026-06-04 05:41:57 +0700

    fix: update stub file to follow Filament return type pattern

--- stubs/ide.php
+++ stubs/ide.php
@@ -2,7 +2,7 @@
 
 namespace Filament\Actions {
     /**
-     * @method $this inlineConfirmation(?string $label = null, int $timeout = 3000)
+     * @method static inlineConfirmation(?string $label = null, int $timeout = 3000)
      * @method bool isInlineConfirmationEligible()
      */
     class Action {}
@@ -10,7 +10,7 @@ namespace Filament\Actions {
 
 namespace Filament\Tables\Actions {
     /**
-     * @method $this inlineConfirmation(?string $label = null, int $timeout = 3000)
+     * @method static inlineConfirmation(?string $label = null, int $timeout = 3000)
      * @method bool isInlineConfirmationEligible()
      */
     class Action {}
@@ -18,7 +18,7 @@ namespace Filament\Tables\Actions {
 
 namespace Filament\Infolists\Components\Actions {
     /**
-     * @method $this inlineConfirmation(?string $label = null, int $timeout = 3000)
+     * @method static inlineConfirmation(?string $label = null, int $timeout = 3000)
      * @method bool isInlineConfirmationEligible()
      */
     class Action {}
